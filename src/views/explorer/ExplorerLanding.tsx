e5adaa68ee5efb10cdeb09308b75a433f212c517
Eric Do <ericdo.617@gmail.com>
2021-07-20 14:12:02 -0700

    Refactor with react-router-dom

---
+++ src/views/explorer/ExplorerLanding.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Dashboard from './DashBoard'
+
+const ExplorerLanding = () => {
+
+  return (
+    <>
+      <Dashboard />
+    </>
+  )
+};
+
+export default ExplorerLanding;
