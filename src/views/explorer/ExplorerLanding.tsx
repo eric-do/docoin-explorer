bf35c0a673df2d35945eca6d681c35958c54f6f5
Eric Do <ericdo.617@gmail.com>
2021-07-20 21:40:34 -0700

    Align titles and subtitles

--- src/views/explorer/ExplorerLanding.tsx
+++ src/views/explorer/ExplorerLanding.tsx
@@ -5,6 +5,15 @@ const ExplorerLanding = () => {
 
   return (
     <>
+      <div className="section">
+        <h1 className="title has-text-left is-1">Bitcoin</h1>
+        <h5 className="subtitle has-text-left is-5">
+            Blockchain information for Bitcoin (BTC) including
+            historical prices, the most recently mined blocks,
+            the mempool size of unconfirmed transactions, and
+            data for the latest transactions.
+          </h5>
+      </div>
       <Dashboard />
     </>
   )
