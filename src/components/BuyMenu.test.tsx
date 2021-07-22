e29a785fc215bd12c887b813b2d826c9fc288e0b
Eric Do <ericdo.617@gmail.com>
2021-07-21 23:28:28 -0700

    Implement Buy Docoin popup menu

--- src/components/BuyMenu.test.tsx
+++ src/components/BuyMenu.test.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import BuyMenu from './BuyMenu';
 
 it('should render image and button', () => {
   render(<BuyMenu />);
-  expect(screen.getByRole('button')).toBeInTheDocument();
+  expect(screen.getByRole('button', { name: 'Buy Docoin' })).toBeInTheDocument();
+});
+
+it('should display menu on button click', () => {
+  render(<BuyMenu />);
+  const button = screen.getByRole('button', { name: 'Buy Docoin' });
+  userEvent.click(button);
+  expect(screen.getByRole('menu')).toBeInTheDocument();
+  expect(screen.getByRole('textbox')).toBeInTheDocument();
+  expect(screen.getByRole('button', { name: 'Continue' })).toBeInTheDocument();
 });
--- src/components/BuyMenu.tsx
+++ src/components/BuyMenu.tsx
@@ -5,17 +5,35 @@ interface ButtonProps {
   hovered: boolean;
 }
 
+interface FormInputs {
+  amount: number;
+  email: string;
+}
+
 const fadeIn = keyframes`
   from { opacity: 0; }
   to   { opacity: 1; }
 `;
 
-const StyledButton = Styled.button<ButtonProps>`
-  max-width: ${props => props.hovered ? "10rem" : 0};
+const NakedInput = Styled.input`
+  border: 0;
+  outline: none;
+`;
+
+const Denomination = Styled.span`
+  color: rgb(152, 161, 178);
+`;
+
+const BuyButton = Styled.button<ButtonProps>`
+  max-width: ${props => props.hovered ? "12rem" : 0};
   -webkit-transition: max-width 0.5s;
   transition: max-width 0.5s;
   display: inline-flex;
 `;
+
+const DropUpMenu = Styled.div`
+  width: 20rem;
+`
 
 const ButtonText = Styled.span`
   animation: ${fadeIn} 0.75s;
@@ -23,18 +41,78 @@ const ButtonText = Styled.span`
 
 const BuyMenu = () => {
   const [hover, setHover] = React.useState<boolean>(false);
+  const [displayMenu, setDisplayMenu] = React.useState<boolean>(false);
+  const [formInputs, setFormInputs] = React.useState<FormInputs>({ amount: 0, email: '' })
+
+  const handleInputs = (e: React.FormEvent<HTMLInputElement>) => {
+    const { name, value } = e.currentTarget;
+    setFormInputs({
+      ...formInputs,
+      [name]: value
+    })
+  }
 
   return (
     <div className="is-flex is-justify-content-flex-end section">
-      <StyledButton
-        hovered={hover}
-        className="button is-rounded is-primary is-large buy-box"
-        onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}
-      >
-          { hover && <ButtonText>Buy Docoin</ButtonText> }
-          { !hover && <span className="icon is-medium"><i className="fas fa-home"></i></span> }
-      </StyledButton>
+      <div className={`dropdown is-right is-up ${displayMenu && 'is-active'}`}>
+        <div className="dropdown-trigger">
+          <BuyButton
+            aria-label="Buy Docoin"
+            hovered={hover}
+            onClick={() => setDisplayMenu(!displayMenu)}
+            aria-haspopup="true"
+            aria-controls="buy-drop-up"
+            className="button is-rounded is-primary is-large buy-box"
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
+          >
+              { displayMenu && <span className="icon is-medium"><i className="fas fa-times"></i></span>}
+              { hover && !displayMenu && <ButtonText>Buy Docoin</ButtonText> }
+              { !hover && !displayMenu && <span className="icon is-medium"><i className="fas fa-home"></i></span> }
+
+          </BuyButton>
+        </div>
+        <DropUpMenu className="dropdown-menu" id="buy-drop-up" role="menu">
+          <div className="dropdown-content">
+            <div className="dropdown-item">
+              <h4 className="title has-text-left is-4">
+                Buy Docoin
+              </h4>
+              <form>
+                <div className="is-flex is-justify-content-flex-start">
+                    <Denomination className="is-size-2">$</Denomination>
+                    <NakedInput
+                      className="is-size-2"
+                      name="amount"
+                      type="number"
+                      min=".01"
+                      placeholder="0"
+                      step=".01"
+                      required
+                      value={formInputs.amount}
+                      onChange={handleInputs}
+                    />
+                </div>
+                <div className="field">
+                  <label className="label has-text-left">
+                    Your Email
+                  </label>
+                  <div className="control">
+                    <input
+                      className="input is-primary"
+                      type="email"
+                      name="email"
+                      value={formInputs.email}
+                      onChange={handleInputs}
+                    />
+                  </div>
+                </div>
+                <button className="button is-primary">Continue</button>
+              </form>
+            </div>
+          </div>
+        </DropUpMenu>
+      </div>
     </div>
   )
 };
